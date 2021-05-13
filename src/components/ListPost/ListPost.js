0e8113f56b2dd5d34ed5986b0232437a5ea69d24
Rendy Artha <rendyartha312@gmail.com>
2021-05-13 09:30:17 +0700

    install react icons, made features likes, and share

--- src/components/ListPost/ListPost.js
+++ src/components/ListPost/ListPost.js
@@ -1,11 +1,23 @@
 import styles from '../../styles/Home.module.scss';
+import { FaHeart } from 'react-icons/fa';
+import { HiShare } from 'react-icons/hi';
 
 const ListPost = () => {
   return (
     <div className={styles.wrapperListPost}>
       <div className={styles.listPost}>
         <h2>Test building Next JS</h2>
-        <p>3/12/2021</p>
+        <div className={styles.description}>
+          <div>
+            <FaHeart />
+            <span>20</span>
+          </div>
+          <div>
+            <HiShare />
+            <span>1</span>
+          </div>
+          <p>3/12/2021</p>
+        </div>
       </div>
       <div className={styles.listPost}>
         <h2>Test building Next JS</h2>
