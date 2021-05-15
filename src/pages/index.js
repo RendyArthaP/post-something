e28370fe04edc1fd5ba3f7ab0f58a014f2ae1c2b
Rendy Artha <rendyartha312@gmail.com>
2021-05-15 22:00:15 +0700

    custom hooks, setup context api

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import Bio from '../components/Bio/Bio'
 import ListPost from '../components/ListPost/ListPost'
 import PostForm from '../components/PostForm/PostForm'
 import styles from '../styles/Home.module.scss'
+import { useAuth } from '../components/Hooks/useAuth'; 
 
 export default function Home() {
+  const auth = useAuth()
+
   return (
     <div className={styles.container}>
       <Head>
